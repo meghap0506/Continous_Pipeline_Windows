1be2a5461aa7931ebc2d3707deedfd0e75fdff3b
meghap0506 <Megha.Pokharkar@communitybrands.com>
2021-11-10 16:26:38 +0530

    Add logic and test

---
+++ src/main/java/com/simplilearn/Calculator_Windows/CalculatorWindowsApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Calculator_Windows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorWindowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorWindowsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/Calculator_Windows/CalculatorWindowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Calculator_Windows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorWindowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
